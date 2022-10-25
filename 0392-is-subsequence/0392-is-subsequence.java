6da6391ae4b81880303a034dfdd1a8493f6c8dfe
Tanmaya jain <tanmayaboss@gmail.com>
2022-10-25 20:06:14 +0530

    Time: 4 ms (30.78%), Space: 42.3 MB (27.34%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
        int j = 0;
-       char[] c = new char[t.length()];
        if (s.length() == 0) {
            return true;
        }
@@ -11,13 +10,12 @@ class Solution {
         for (int i = 0; i < t.length(); i++) {
             if (t.charAt(i) == s.charAt(j))
             {
-               c[j] = t.charAt(i);
                j++;
                 if (j == s.length()) {
                     return true;
                 }
             }
         }
-        return j == s.length();
+        return false;
 }
 }
