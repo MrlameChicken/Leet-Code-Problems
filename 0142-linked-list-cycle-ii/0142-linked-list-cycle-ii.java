b79d0ae725d1a2a5f53c03e1cc85678b9068003b
Tanmaya jain <tanmayaboss@gmail.com>
2022-11-06 22:20:23 +0530

    Time: 3 ms (49.50%), Space: 44.9 MB (65.49%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -10,23 +10,42 @@
  * }
  */
 public class Solution {
-    public ListNode detectCycle(ListNode head) {
-        HashSet<ListNode> set = new HashSet<ListNode>();
-        while (head != null) {
-            // If we have already has this node
-            // in hashmap it means their is a cycle
-            // (Because you we encountering the
-            // node second time).
-            if (set.contains(head))
-                return head;
+//     public ListNode detectCycle(ListNode head) {
+//         HashSet<ListNode> set = new HashSet<ListNode>();
+//         while (head != null) {
+//             // If we have already has this node
+//             // in hashmap it means their is a cycle
+//             // (Because you we encountering the
+//             // node second time).
+//             if (set.contains(head))
+//                 return head;
  
-            // If we are seeing the node for
-            // the first time, insert it in hash
-            set.add(head);
+//             // If we are seeing the node for
+//             // the first time, insert it in hash
+//             set.add(head);
  
-            head = head.next;
+//             head = head.next;
+//         }
+ 
+//         return null;
+//     }
+     public ListNode detectCycle(ListNode head) {
+        ListNode slow=head,fast=head;
+
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+
+            if(slow==fast){
+                slow=head;
+
+                while(slow!=fast){
+                    slow=slow.next;
+                    fast=fast.next;
+                }
+                return slow;
+            }
         }
- 
         return null;
     }
 }
