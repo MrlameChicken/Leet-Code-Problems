4f171803f3136145b91fcd8fb8fe01b57240fba3
Tanmaya jain <tanmayaboss@gmail.com>
2022-11-06 20:58:37 +0530

    Time: 18 ms (6.33%), Space: 226.7 MB (31.27%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
@@ -9,27 +9,31 @@
  * }
  */
 class Solution {
-    public ListNode deleteMiddle(ListNode head) {
+   public ListNode deleteMiddle(ListNode head) {
         ListNode currentNode = head;
         ListNode prev = null;
-        //if single elemnet in list
-        if (currentNode.next == null) {
-            currentNode = null;
-            return currentNode;
-        }
-        
+
+        if(head ==null){
+             return head;
+         }
+
+         ListNode mid=middleNode(head);
+         if(head==mid){
+             return null;
+         }
+
         //find middle
         ListNode middle = middleNode(head);
-        
+
         while (currentNode != null && currentNode != middle) {
            prev = currentNode;
            currentNode = currentNode.next;
         }
-        
+
         if (currentNode!= null) {
             prev.next = currentNode.next;
         }
-        
+
         return head;
     }
     
@@ -38,9 +42,30 @@ class Solution {
         ListNode fast = head;
         
         while (fast !=null && fast.next !=null) {
+       
             fast = fast.next.next;
             slow = slow.next;
         }
+       
+        
         return slow;
     }
+    
+//             if(head ==null || head.next==null){
+//             return null;
+//         }
+//         ListNode dummy=new ListNode(0);
+//         dummy.next=head;
+    
+//         ListNode fast=head;
+//         ListNode slow=head;
+//         ListNode prev=null;
+//         while(fast!=null && fast.next!=null){
+//             prev=slow;
+//             slow=slow.next;
+//             fast=fast.next.next;
+//         }
+//         prev.next=prev.next.next;
+//         return dummy.next;
+//}
 }
