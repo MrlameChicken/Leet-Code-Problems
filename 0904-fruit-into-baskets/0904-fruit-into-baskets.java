ab914508c86b97195339e10c5d894c66877a5836
Tanmaya jain <tanmayaboss@gmail.com>
2023-10-21 15:05:49 +0530

    Time: 48 ms (41.44%), Space: 55.1 MB (14.66%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -6,23 +6,20 @@ class Solution {
         int max = 0;
         
         while(j<fruits.length) {
-           
-            if(map.size()==2 && !map.containsKey(fruits[j]))
-            {
-                while(i<j && map.size()>=2) {
+            
+            map.put(fruits[j],map.getOrDefault(fruits[j],0)+1);
+            
+             while(map.size()>2) {
                     map.put(fruits[i],map.get(fruits[i])-1);
                     if (map.get(fruits[i]) == 0) {
                         map.remove(fruits[i]);
                     }
                     i++;
-                }
-            } else {
-                map.put(fruits[j],map.getOrDefault(fruits[j],0)+1);
+            }
+                
                 max = Math.max(max,j-i+1);
                 j++;
             }
-            
-        }
         
         return max;
     }
