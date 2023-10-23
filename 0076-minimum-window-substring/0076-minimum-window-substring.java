a78a59aa14a3bf1e48cda019e1c1dd7d9f474027
Tanmaya jain <tanmayaboss@gmail.com>
2023-10-23 09:21:24 +0530

    Time: 17 ms (42.57%), Space: 44 MB (59.66%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,45 +1,43 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int i = 0;
+            int i = 0;
         int j = 0;
         Map<Character,Integer> map = new HashMap<>();
+        int count;
         String ans = "";
         int min = Integer.MAX_VALUE;
-        
-        
+
         for(char c:t.toCharArray()) {
             map.put(c,map.getOrDefault(c,0)+1);
         }
         
-        int count = map.size();
-        
+        count = map.size();
+
         while(j<s.length()) {
-            if (map.get(s.charAt(j))!=null) {
+            if (map.get(s.charAt(j)) != null) {
                 map.put(s.charAt(j),map.get(s.charAt(j))-1);
                 if (map.get(s.charAt(j)) == 0) {
                     count--;
                 }
             }
-            
-           
-            
+
             if (count == 0) {
-                 while (!map.containsKey(s.charAt(i)) || map.get(s.charAt(i)) < 0) {
-                    if (map.containsKey(s.charAt(i))) {
+                while(map.get(s.charAt(i)) == null || map.get(s.charAt(i)) < 0) {
+                    if (map.get(s.charAt(i))!=null) {
                         map.put(s.charAt(i),map.get(s.charAt(i))+1);
-                    }  
-                    
-                     i++;  
+                    }
+                    i++;
                 }
                 
-                if (min>j-i+1) {
-                    ans = s.substring(i,j+1);
+                if(min>j-i+1) {
                     min = j-i+1;
+                    ans = s.substring(i,j+1);
                 }
                 
             }
             j++;
         }
+
         return ans;
         
     }
