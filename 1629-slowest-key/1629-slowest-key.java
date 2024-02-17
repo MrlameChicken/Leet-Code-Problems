f857255585c4d90390efaa1e40293dda63c9eed2
Tanmaya jain <tanmayaboss@gmail.com>
2024-02-17 22:39:02 +0530

    Time: 1 ms (99.74%), Space: 44.6 MB (28.50%) - LeetHub

---
+++ 1629-slowest-key/1629-slowest-key.java
@@ -0,0 +1,20 @@
+class Solution {
+    public char slowestKey(int[] releaseTimes, String keysPressed) {
+        int keyTime = releaseTimes[0];
+        char keyPressed = keysPressed.charAt(0);
+        
+        for (int i=1;i<releaseTimes.length;i++) {
+            int diff = releaseTimes[i] - releaseTimes[i-1];
+            if (diff > keyTime) {
+                keyTime = diff;
+                keyPressed = keysPressed.charAt(i);
+            }
+            if (diff == keyTime && keyPressed < keysPressed.charAt(i)) {
+                 keyTime = diff;
+                keyPressed = keysPressed.charAt(i);
+            }
+        }
+        
+        return keyPressed;
+    }
+}
